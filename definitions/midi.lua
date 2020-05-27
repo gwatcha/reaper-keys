ae36cd5e8acd7b8a84f46dc1245db65e916c96d8
molleweide <hjalmar.jakobsson@telia.com>
2020-05-27 22:10:47 +0200

    adds actions for setting midi note lengths. It also adds a midi mapping but I don't succeed in making it work.

--- definitions/midi.lua
+++ definitions/midi.lua
@@ -46,6 +46,24 @@ return {
     ["<M-J>"] = "MoveNoteDownOctave",
     ["<M-l>"] = "MoveNoteRight",
     ["<M-h>"] = "MoveNoteLeft",
+    ["<C-G>"] = {"+set note length", {
+        ["z"] = "SetMidiNoteLengthTo1By1",
+        ["x"] = "SetMidiNoteLengthTo1By2",
+        ["c"] = "SetMidiNoteLengthTo1By2dot",
+        ["v"] = "SetMidiNoteLengthTo1By2trip",
+        ["b"] = "SetMidiNoteLengthTo1By4",
+        ["a"] = "SetMidiNoteLengthTo1By4dot",
+        ["s"] = "SetMidiNoteLengthTo1By4trip",
+        ["d"] = "SetMidiNoteLengthTo1By8",
+        ["f"] = "SetMidiNoteLengthTo1By8dot",
+        ["g"] = "SetMidiNoteLengthTo1By8trip",
+        ["q"] = "SetMidiNoteLengthTo1By16",
+        ["w"] = "SetMidiNoteLengthTo1By16dot",
+        ["e"] = "SetMidiNoteLengthTo1By16trip",
+        ["r"] = "SetMidiNoteLengthTo1By32",
+        ["t"] = "SetMidiNoteLengthTo1By64",
+    }}, -- i set it to B because I am not really sure which keys are available
   },
 }
+-- ["<SPC>"] = { "+leader commands", {
 
