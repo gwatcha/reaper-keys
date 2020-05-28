6580f9acbb6ff2f5ce13a85effe149be0be8074b
molleweide <hjalmar.jakobsson@telia.com>
2020-05-28 18:25:59 +0200

    adds additional actions for set midi grid and key binding.

--- definitions/global.lua
+++ definitions/global.lua
@@ -68,7 +68,7 @@ return {
     [">"] = "ShiftTimeSelectionRight",
     ["v"] = "SetModeVisualTimeline",
     ["<"] = "ShiftTimeSelectionLeft",
-    ["<C-SPC>"] = "ToggleViewMixer",
+    ["<CM-SPC>"] = "ToggleViewMixer",
     ["<ESC>"] = "Reset",
     ["<return>"] = "StartStop",
     ["<M-T>"] = "MoveToMouseAndPlay",
@@ -246,33 +246,98 @@ return {
                 ["d"] = "CloseProject",
                 ["x"] = "CleanProjectDirectory",
             }},
-    ["G"] = {"+grid",{
-            ["z"] = "GridSetTo1by1", -- low left hand
-            ["x"] = "GridSetTo1by2",
-            ["c"] = "GridSetTo1by3",
-            ["v"] = "GridSetTo1by4",
-            ["b"] = "GridSetTo1by5",
+    ["G"] = {"+grid",{ -- !!bug: for some reason some of these commands also execute other commands.
+            ["m"] = { "+midi grid", {
+                ["z"] = "GridMidiSetTo1by1", -- low left hand
+                ["x"] = "GridMidiSetTo1by2",
+                ["c"] = "GridMidiSetTo1by3",
+                ["v"] = "GridMidiSetTo1by4", -- for some rear
+                ["b"] = "GridMidiSetTo1by5",
 
-            ["a"] = "GridSetTo1by6", -- mid left hand
-            ["s"] = "GridSetTo1by7",
-            ["d"] = "GridSetTo1by8",
-            ["f"] = "GridSetTo1by9",
-            ["g"] = "GridSetTo1by10",
+                ["a"] = "GridMidiSetTo1by6", -- mid left hand
+                ["s"] = "GridMidiSetTo1by7",
+                ["d"] = "GridMidiSetTo1by8",
+                ["f"] = "GridMidiSetTo1by9",
+                ["g"] = "GridMidiSetTo1by10",
 
-            ["q"] = "GridSetTo1by12", -- upper left hand
-            ["w"] = "GridSetTo1by16",
-            ["e"] = "GridSetTo1by18",
-            ["r"] = "GridSetTo1by24",
-            ["t"] = "GridSetTo1by32",
+                ["q"] = "GridMidiSetTo1by12", -- upper left hand
+                ["w"] = "GridMidiSetTo1by16",
+                ["e"] = "GridMidiSetTo1by18",
+                ["r"] = "GridMidiSetTo1by24",
+                ["t"] = "GridMidiSetTo1by32",
 
-            ["n"] = "GridSetTo1by48", -- lower right
-            ["m"] = "GridSetTo1by64",
-            [","] = "GridSetTo1by128",
-            ["."] = "GridSetTo2by1",
-            ["/"] = "GridSetTo2by3",
+                ["n"] = "GridMidiSetTo1by48", -- lower right
+                ["m"] = "GridMidiSetTo1by64",
+                [","] = "GridMidiSetTo1by128",
+                ["."] = "GridMidiSetTo2by1",
+                ["/"] = "GridMidiSetTo2by3",
 
-            ["h"] = "GridSetTo3by1", -- mid right
-            ["j"] = "GridSetTo4by1",
+                ["h"] = "GridMidiSetTo3by1", -- mid right
+                ["j"] = "GridMidiSetTo4by1",
+
+                ["y"] = "GridMidiSetGridTypeDotted", -- top right
+                ["u"] = "GridMidiSetGridTypeStraight",
+                ["i"] = "GridMidiSetGridTypeSwing",
+                ["o"] = "GridMidiSetGridTypeTriplet",
+            }},
+            ["t"] = {"+main grid", {
+                ["z"] = "GridSetTo1by1", -- low left hand
+                ["x"] = "GridSetTo1by2",
+                ["c"] = "GridSetTo1by3",
+                ["v"] = "GridSetTo1by4",
+                ["b"] = "GridSetTo1by5",
+
+                ["a"] = "GridSetTo1by6", -- mid left hand
+                ["s"] = "GridSetTo1by7",
+                ["d"] = "GridSetTo1by8",
+                ["f"] = "GridSetTo1by9",
+                ["g"] = "GridSetTo1by10",
+
+                ["q"] = "GridSetTo1by12", -- upper left hand
+                ["w"] = "GridSetTo1by16",
+                ["e"] = "GridSetTo1by18",
+                ["r"] = "GridSetTo1by24",
+                ["t"] = "GridSetTo1by32",
+
+                ["n"] = "GridSetTo1by48", -- lower right
+                ["m"] = "GridSetTo1by64",
+                [","] = "GridSetTo1by128",
+                ["."] = "GridSetTo2by1",
+                ["/"] = "GridSetTo2by3",
+
+                ["h"] = "GridSetTo3by1", -- mid right
+                ["j"] = "GridSetTo4by1",
+
+            }},
+            ["a"] = {"+both grids", {
+                ["z"] = "GridAllSetTo1by1", -- low left hand
+                ["x"] = "GridAllSetTo1by2",
+                ["c"] = "GridAllSetTo1by3",
+                ["v"] = "GridAllSetTo1by4",
+                ["b"] = "GridAllSetTo1by5",
+
+                ["a"] = "GridAllSetTo1by6", -- mid left hand
+                ["s"] = "GridAllSetTo1by7",
+                ["d"] = "GridAllSetTo1by8",
+                ["f"] = "GridAllSetTo1by9",
+                ["g"] = "GridAllSetTo1by10",
+
+                ["q"] = "GridAllSetTo1by12", -- upper left hand
+                ["w"] = "GridAllSetTo1by16",
+                ["e"] = "GridAllSetTo1by18",
+                ["r"] = "GridAllSetTo1by24",
+                ["t"] = "GridAllSetTo1by32",
+
+                ["n"] = "GridAllSetTo1by48", -- lower right
+                ["m"] = "GridAllSetTo1by64",
+                [","] = "GridAllSetTo1by128",
+                ["."] = "GridAllSetTo2by1",
+                ["/"] = "GridAllSetTo2by3",
+
+                ["h"] = "GridAllSetTo3by1", -- mid right
+                ["j"] = "GridAllSetTo4by1",
+
+            }},
         }},
     }},
   },
