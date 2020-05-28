b59cecf66344a441d2ad4e2421aeb869fbe91080
mostlysimlar <michael.ampm@gmail.com>
2020-05-28 13:30:16 -0700

    add configuration file

---
+++ definitions/config.lua
@@ -0,0 +1,7 @@
+return {
+  -- should operators in visual modes reset the selection or have it persist?
+  persist_visual_timeline_selection = true,
+  persist_visual_track_selection = false,
+  -- options in decreasing verbosity: [trace debug info warn user error fatal]
+  log_level = 'user',
+}
