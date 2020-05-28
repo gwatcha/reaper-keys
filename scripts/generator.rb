3d67ec4a9e8f7b9203f9421dcace643d8ca6bcd1
mostlysimlar <michael.ampm@gmail.com>
2020-05-27 20:56:11 -0700

    fix: map shifted unshifted-keys to their shift equivalents (S-1 -> !)

--- scripts/generator.rb
+++ scripts/generator.rb
@@ -19,7 +19,7 @@ class Generator
       key = "<#{modifier_keys_excluding_shift}-#{letter.upcase}>"
       key_name = "(#{key_mod}-#{letter})"
     end
-    return key, key_name
+    [key, key_name]
   end
 
   def format_modded_key(key, key_name, key_table_name, key_mod)
@@ -42,30 +42,52 @@ class Generator
 
   def gen_modified_keys(key, key_id, key_name, key_table_name, context, context_id)
     KeyDefinitions::KEY_MODS.each do |key_mod, key_mod_id|
-      next if %i[shifted normal].include?(key_table_name) &&
+      next if %i[shifted].include?(key_table_name) &&
               %i[S MS CS CMS].include?(key_mod)
 
-      modded_key, modded_key_name = format_modded_key(key, key_name, key_table_name, key_mod)
+      # reuse the scripts for shifted keys that have shift characters (e.g. <S-1> -> !)
+      # but still put the keymap key lines in due to differences in how OS see '!' (as <S-1> or !)
+      if KeyDefinitions::SHIFT_MAP[key] && %i[S MS CS CMS].include?(key_mod)
+        shifted_key = KeyDefinitions::SHIFT_MAP[key]
+        key_mod = key_mod[/([CM]+)S/, 1]
+        modded_key = shifted_key
+        if key_mod
+          modded_key, _ = format_modded_key(shifted_key, shifted_key, 'shifted', key_mod)
+        end
+
+        if KeyDefinitions::MOD_DECREMENTED_KEYS.detect { |x| x == key }
+          key_mod_id -= 1
+        end
+
+        reaper_key_script_id = "_reaper_keys_#{context}_#{modded_key}"
+        put_keymap_key_line(key_mod_id, key_id, context_id, reaper_key_script_id)
+        next
+      end
 
       if KeyDefinitions::MOD_DECREMENTED_KEYS.detect { |x| x == key }
         key_mod_id -= 1
       end
 
+      modded_key, modded_key_name = format_modded_key(key, key_name, key_table_name, key_mod)
+
       gen_key(key_mod_id, modded_key, modded_key_name, key_id, context, context_id)
     end
   end
 
-  def format_keymap_scr_line(key, context_id, script_id, key_script_path)
+  def put_keymap_scr_line(key, context_id, script_id, key_script_path)
     desc = "[reaper-keys] [key_press] [#{key}]"
+    scr_line = nil
     if key[/"/]
-      "SCR 4 #{context_id} \'#{script_id}\' \'#{desc}\' #{key_script_path}\n"
+      scr_line = "SCR 4 #{context_id} \'#{script_id}\' \'#{desc}\' #{key_script_path}\n"
     else
-      "SCR 4 #{context_id} \"#{script_id}\" \"#{desc}\" #{key_script_path}\n"
+      scr_line = "SCR 4 #{context_id} \"#{script_id}\" \"#{desc}\" #{key_script_path}\n"
     end
+    open(@keymap_path, 'a') { |file| file.puts scr_line }
   end
 
-  def format_keymap_key_line(key_type_id, key_id, context_id, script_id)
-    "KEY #{key_type_id} #{key_id} #{script_id} #{context_id}\n"
+  def put_keymap_key_line(key_type_id, key_id, context_id, script_id)
+    key_line = "KEY #{key_type_id} #{key_id} #{script_id} #{context_id}\n"
+    open(@keymap_path, 'a') { |file| file.puts key_line }
   end
 
   def gen_key_script(key, context, path)
@@ -90,11 +112,9 @@ local doInput = require('internal.reaper-keys')
 
     reaper_key_script_id = "_reaper_keys_#{context}_#{key}"
     reaper_script_path = './' + @root_dir_path + script_path
-    scr_line = format_keymap_scr_line(key, context_id, reaper_key_script_id, reaper_script_path)
-    key_line = format_keymap_key_line(key_type_id, key_id, context_id, reaper_key_script_id)
 
-    keymap_lines = scr_line + key_line
-    open(@keymap_path, 'a') { |file| file.puts keymap_lines }
+    put_keymap_scr_line(key, context_id, reaper_key_script_id, reaper_script_path)
+    put_keymap_key_line(key_type_id, key_id, context_id, reaper_key_script_id)
   end
 
   def gen_interface
@@ -103,6 +123,7 @@ local doInput = require('internal.reaper-keys')
         unmodded_key_type_id = key_table[:key_type_id]
         key_table[:keys].each do |key, key_id|
           key_name = key
+
           if KeyDefinitions::ALIASES[key]
             key_name = KeyDefinitions::ALIASES[key]
           end
