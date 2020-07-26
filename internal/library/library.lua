dfaabebe5dc318eaa9c1489f47b15e7d60cff6c6
mostlysimlar <michael.ampm@gmail.com>
2020-07-26 09:52:34 -0700

    move state library commands into submodule

--- internal/library/library.lua
+++ internal/library/library.lua
@@ -1,51 +1,13 @@
 local state_interface = require('state_machine.state_interface')
-local custom_actions = require('custom_actions')
 local reaper_utils = require('custom_actions.utils')
 
 local log = require('utils.log')
 local marks = require('library.marks')
+local state = require('library.state')
 
 local library = {}
 library.marks = marks
-
-function library.setModeNormal()
-  state_interface.setMode('normal')
-end
-
-function library.setModeVisualTrack()
-  local current_track = reaper.GetLastTouchedTrack()
-  if current_track then
-    reaper.SetOnlyTrackSelected(current_track)
-
-    local visual_track_pivot_i = reaper.GetMediaTrackInfo_Value(current_track, "IP_TRACKNUMBER") - 1
-
-    state_interface.setMode('visual_track')
-    state_interface.setVisualTrackPivotIndex(visual_track_pivot_i)
-  end
-end
-
-function library.setModeVisualTimeline()
-  local current_position = reaper.GetCursorPosition()
-  reaper.GetSet_LoopTimeRange(true, false, current_position, current_position, false)
-  state_interface.setMode('visual_timeline')
-
-  if state_interface.getTimelineSelectionSide() == 'left' then
-    state_interface.setTimelineSelectionSide('right')
-  end
-end
-
-function library.switchTimelineSelectionSide()
-  local go_to_start_of_selection = 40630
-  local go_to_end_of_selection = 40631
-
-  if state_interface.getTimelineSelectionSide() == 'right' then
-    reaper.Main_OnCommand(go_to_start_of_selection, 0)
-    state_interface.setTimelineSelectionSide('left')
-  else
-    reaper.Main_OnCommand(go_to_end_of_selection, 0)
-    state_interface.setTimelineSelectionSide('right')
-  end
-end
+library.state = state
 
 function library.matchTrackNameBackward()
   local _, name = reaper.GetUserInputs("Match Backward", 1, "Match String", "")
---
+++ internal/library/state.lua
@@ -0,0 +1,44 @@
+local state_interface = require('state_machine.state_interface')
+
+local state = {}
+
+function state.setModeNormal()
+  state_interface.setMode('normal')
+end
+
+function state.setModeVisualTrack()
+  local current_track = reaper.GetLastTouchedTrack()
+  if current_track then
+    reaper.SetOnlyTrackSelected(current_track)
+
+    local visual_track_pivot_i = reaper.GetMediaTrackInfo_Value(current_track, "IP_TRACKNUMBER") - 1
+
+    state_interface.setMode('visual_track')
+    state_interface.setVisualTrackPivotIndex(visual_track_pivot_i)
+  end
+end
+
+function state.setModeVisualTimeline()
+  local current_position = reaper.GetCursorPosition()
+  reaper.GetSet_LoopTimeRange(true, false, current_position, current_position, false)
+  state_interface.setMode('visual_timeline')
+
+  if state_interface.getTimelineSelectionSide() == 'left' then
+    state_interface.setTimelineSelectionSide('right')
+  end
+end
+
+function state.switchTimelineSelectionSide()
+  local go_to_start_of_selection = 40630
+  local go_to_end_of_selection = 40631
+
+  if state_interface.getTimelineSelectionSide() == 'right' then
+    reaper.Main_OnCommand(go_to_start_of_selection, 0)
+    state_interface.setTimelineSelectionSide('left')
+  else
+    reaper.Main_OnCommand(go_to_end_of_selection, 0)
+    state_interface.setTimelineSelectionSide('right')
+  end
+end
+
+return state
