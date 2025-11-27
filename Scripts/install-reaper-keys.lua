10c0e806de6587bf190a12ecb0f1e8bc995bb02a
Mikhail Kot <to@myrrc.dev>
2025-11-27 10:54:28 +0000

    version

--- Scripts/install-reaper-keys.lua
+++ Scripts/install-reaper-keys.lua
@@ -1,5 +1,5 @@
 -- @description reaper-keys: map keystroke combinations to actions like in vim
--- @version 2.0.0-a15
+-- @version 2.0.0-a16
 -- @author gwatcha
 -- @links
 --   GitHub repository https://github.com/gwatcha/reaper-keys
